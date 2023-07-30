31523372730cea88dae4b4de4b4d8840214d50da
Abu Zafar Emon <zafar emon>
2023-07-30 21:47:09 +0600

    setup header and footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Shared/Header/Header";
+import Footer from "../components/Shared/Footer/Footer";
 
 const Main = () => {
   return (
     <div>
+      <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
